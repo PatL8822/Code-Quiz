f1000b5a2db77b58e409db8f382ce6fb3d2674c8
Isaiah411013 <Archangel_r320@hotmail.com>
2022-09-11 13:18:52 -0700

    overhual of all code for functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,16 @@
 const ans2 = document.getElementById("ansTwo");
 const ans3 = document.getElementById("ansThree");
 const ans4 = document.getElementById("ansFour");
-var question = document.getElementById("question");
+const questionEl = document.getElementById("questionEl");
 var ans1 = document.getElementById("ansOne");
 var hideH2 = document.getElementById("hide-two");
-var container = document.getElementById("hide");
+var containerEl = document.getElementById("question-container");
 var h1 = document.querySelectorAll("h1");
 var startButton = document.querySelector("#strBtn");
 var countDown = document.getElementById("time");
 var confirmMsg = document.getElementById("confirm");
+var highScore = document.getElementById('highScor');
+var timerText = document.getElementById('timerText');
 var time = 100;
 var score = [];
 var ifTimeEnds = [];
@@ -35,7 +37,7 @@ h1[0].textContent = "~ Quiz ~";
 startButton.textContent = "Start";
 startButton.setAttribute("style", "font-size: 70px; background-color: black; color: white; border-radius: 15px; cursor: pointer;");
 
-//adding eventlistener to star quiz
+//adding eventlistener to star quiz and timer
 startButton.addEventListener("click", quiztime);
 
 // this function deals with the timer
@@ -43,111 +45,77 @@ function quiztime() {
     var startTime = setInterval(function () {
         time--;
         countDown.innerHTML = time
-
         if (time === 0) {
             clearInterval(startTime);
         }
         quize()
     }, 1000);
-
 }
 
-// this function is hiding the str btn and then presents the quize
+// this function shows the quiz questions and hides the str btn and high scors btn and ulilising the .classList()
 function quize() {
-    hideH2.setAttribute("style", "display: none;")
-    startButton.setAttribute("style", "display: none;")
-    container.setAttribute("style", "display: flex;")
-    questionOne()
+    timerText.classList.remove('hide');
+    highScore.classList.add('hide')
+    hideH2.setAttribute("style", "display: none;");
+    startButton.setAttribute("style", "display: none;");
+    containerEl.classList.remove('hide');
+    selectFirstQuestion()
 }
 
-function questionOne() {
-
-    question.textContent = "What elements MUST you have in HTML ?"
-
-    ans1.textContent = "A. body, html, title, head, DOCTYPE, header"
-    ans2.textContent = "B. body, main, title, head, DOCTYPE, header"
-    ans3.textContent = "C. head, html, DOCTYPE, body, title, meta"
-    ans4.textContent = "D. title, html, body, head, link, footer"
-
-    ans1.addEventListener("click", falsee)
-    ans2.addEventListener("click", falsee)
-    ans3.addEventListener("click", truee)
-    ans4.addEventListener("click", falsee)
-
-    setTimeout(function () {
-        questionTwo()
-    }, 810);
+function selectFirstQuestion(){
+    showQuestions()
 
 }
 
-function questionTwo(){
-
-    question.textContent = "What is a variable?"
-
-    ans1.textContent = "A. Event"
-    ans2.textContent = "B. Container"
-    ans3, textContent = "C. Element"
-    ans4.textContent = "D. Both B and C"
-
-    ans1.addEventListener("click", falsee)
-    ans2.addEventListener("click", falsee)
-    ans3.addEventListener("click", falsee)
-    ans4.addEventListener("click", truee)
+function showQuestions(question){
 
 }
 
-/*function questionThree{
-   question.textContent = ""
-   ans1.textContent = ""
-   ans1 = false
-   ans2.textContent = ""
-   ans2 = false
-   ans3,textContent = ""
-   ans3 = true
-   ans4.textContent = ""
-   ans4 = false
-}
-
-function questionFour{
-   question.textContent = ""
-   ans1.textContent = ""
-   ans1 = false
-   ans2.textContent = ""
-   ans2 = false
-   ans3,textContent = ""
-   ans3 = true
-   ans4.textContent = ""
-   ans4 = false
-}
-
-function questionFive{
-   question.textContent = ""
-   ans1.textContent = ""
-   ans1 = false
-   ans2.textContent = ""
-   ans2 = false
-   ans3,textContent = ""
-   ans3 = true
-   ans4.textContent = ""
-   ans4 = false
-}*/
-
-//function truee and falsee are actions done by clicking on the quiz buttons
-function truee() {
-    score.push(20)
-    document.getElementById("confirm").innerHTML = confirmMsg.textContent = "Correct!!!";
-    setTimeout(function () {
-        document.getElementById("confirm").innerHTML = '';
-    }, 800);
-}
-
-function falsee() {
-    ifTimeEnds.push(1)
-    time -= 5;
-    document.getElementById("confirm").innerHTML = confirmMsg.textContent = "Wrong";
-    setTimeout(function () {
-        document.getElementById("confirm").innerHTML = '';
-    }, 800);
-    setTimeout(function () {
-    }, 800);
-}
+// This is where im making an array list of objecs for the questions.
+const questions = [
+    {
+        question: "What is a variable?",
+        answer:[
+            {text: 'A. Event', correct: false },
+            {text: 'B. Container', correct: false },
+            {text: 'C. Element', correct: false },
+            {text: 'D. Both B and C', correct: true },
+        ]
+    },
+    {
+        question: "How do you call an HTML id IN css?",
+        answer:[
+            {text:'A. #tag', correct: true },
+            {text:'B. .tag', correct: false },
+            {text:'C. tag', correct: false },
+            {text:'D. None of the above', correct: false },
+        ]
+    },
+    {
+        question: "What is the value of  a boolean ?",
+        answer:[
+            {text: 'A. Undefined', correct: false },
+            {text: 'B. NaN', correct: false },
+            {text: 'C. Number', correct: false },
+            {text: 'D. true / false', correct: true },
+        ]
+    },
+    {
+        question: "How do you select all tag elements using Js. ?",
+        answer:[
+            {text:'A. getElementById()', correct: false },
+            {text:'B. querySelectorAll()', correct: true },
+            {text:'C. querySelector()', correct: false },
+            {text:'D. None of the above', correct: false },
+        ]
+    },
+    {
+        question: "What elements MUST you have in HTML ?",
+        answer:[
+            {text:"A. body, html, title, head, DOCTYPE, header", correct: false },
+            {text:"B. body, main, title, head, DOCTYPE, header", correct: false },
+            {text:"C. head, html, DOCTYPE, body, title, meta", correct: true },
+            {text:"D. title, html, body, head, link, footer", correct: false },
+        ]
+    },
+];
