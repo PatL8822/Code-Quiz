93b479df4b79ed27692a3485fea11685dbd29e15
Isaiah411013 <Archangel_r320@hotmail.com>
2022-09-11 21:24:33 -0700

    Updating files almost done with this quiz thing

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,11 +12,8 @@
 11. THEN I can save my initials and my score*/
 
 // below are all the containers needed for JS
-const ans2 = document.getElementById("ansTwo");
-const ans3 = document.getElementById("ansThree");
-const ans4 = document.getElementById("ansFour");
-const questionEl = document.getElementById("questionEl");
-var ans1 = document.getElementById("ansOne");
+var questionEl = document.getElementById("question");
+const answerBtn = document.getElementById('answer-buttons');
 var hideH2 = document.getElementById("hide-two");
 var containerEl = document.getElementById("question-container");
 var h1 = document.querySelectorAll("h1");
@@ -25,10 +22,14 @@ var countDown = document.getElementById("time");
 var confirmMsg = document.getElementById("confirm");
 var highScore = document.getElementById('highScor');
 var timerText = document.getElementById('timerText');
+const span = document.getElementById('span');
+const setScore = document.getElementById('quizeScore');
+// the (let questionIndex) is undefined, need the cotainer so it can be redefined later in the code.
+let questionIndex, currentQuestionIndex;
 var time = 100;
-var score = [];
-var ifTimeEnds = [];
-ifTimeEnds * 20
+var timeTwo = 1;
+var quizeScore = 0;
+var finalScore
 
 // setting the text h1 tag
 h1[0].textContent = "~ Quiz ~";
@@ -48,74 +49,163 @@ function quiztime() {
         if (time === 0) {
             clearInterval(startTime);
         }
-        quize()
     }, 1000);
+    quize()
 }
 
 // this function shows the quiz questions and hides the str btn and high scors btn and ulilising the .classList()
 function quize() {
+    questionIndex = questions;
+    currentQuestionIndex = 0
     timerText.classList.remove('hide');
     highScore.classList.add('hide')
-    hideH2.setAttribute("style", "display: none;");
-    startButton.setAttribute("style", "display: none;");
+    hideH2.classList.add('hide');
+    startButton.classList.add('hide');
     containerEl.classList.remove('hide');
-    selectFirstQuestion()
+    setQuestion()
 }
 
-function selectFirstQuestion(){
-    showQuestions()
+function setQuestion() {
+    btnswap()
+    showQuestions(questionIndex[currentQuestionIndex])
 
 }
 
-function showQuestions(question){
+function showQuestions(questions) {
+    // questionEl.innerText is grabbing the question in the objec array
+    questionEl.innerText = questions.question
+    // for the rest of the code in this function, the answers are called, 
+    //then a button is created for each answer in the answer array, 
+    //the button is then equped with a listener,
+    //finally it is appended to the answers.
+    questions.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('qbtn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct
+        };
+        button.addEventListener('click', selectAnswer);
+        answerBtn.appendChild(button);
+    });
+}
 
+function selectAnswer(e) {
+    const selectBtn = e.target;
+    const correct = selectBtn.dataset.correct;
+    if (correct) {
+        quizeScore = quizeScore + 20;
+        currentQuestionIndex++
+        turee()
+    } else {
+        time -= 5
+        currentQuestionIndex++
+        falsee()
+    };
+    if (currentQuestionIndex >= 5){
+        thatsIt()
+    }
+}
+
+function turee() {
+    document.getElementById("confirm").innerHTML = confirmMsg.textContent = "Correct!!!";
+    setTimeout(function () {
+        document.getElementById("confirm").innerHTML = '';
+    }, 800);
+    setQuestion()
+}
+
+function falsee() {
+    document.getElementById("confirm").innerHTML = confirmMsg.textContent = "Wrong";
+    setTimeout(function () {
+        document.getElementById("confirm").innerHTML = '';
+    }, 800);
+    setQuestion()
+}
+
+// this function eleminates the previous buttins from the HTML id answer-buttons as the new buttons were
+// created in the previous showQuestions  function
+function btnswap() {
+    while (answerBtn.firstChild) {
+        answerBtn.removeChild(answerBtn.firstChild)
+    }
 }
 
 // This is where im making an array list of objecs for the questions.
 const questions = [
     {
-        question: "What is a variable?",
-        answer:[
-            {text: 'A. Event', correct: false },
-            {text: 'B. Container', correct: false },
-            {text: 'C. Element', correct: false },
-            {text: 'D. Both B and C', correct: true },
+        question: 'What is a variable?',
+        answers: [
+            { text: 'A. Event', correct: false },
+            { text: 'B. Container', correct: false },
+            { text: 'C. Element', correct: false },
+            { text: 'D. Both B and C', correct: true },
         ]
     },
     {
-        question: "How do you call an HTML id IN css?",
-        answer:[
-            {text:'A. #tag', correct: true },
-            {text:'B. .tag', correct: false },
-            {text:'C. tag', correct: false },
-            {text:'D. None of the above', correct: false },
+        question: "How do you call an HTML id='tag' IN css?",
+        answers: [
+            { text: 'A. #tag', correct: true },
+            { text: 'B. .tag', correct: false },
+            { text: 'C. tag', correct: false },
+            { text: 'D. None of the above', correct: false },
         ]
     },
     {
         question: "What is the value of  a boolean ?",
-        answer:[
-            {text: 'A. Undefined', correct: false },
-            {text: 'B. NaN', correct: false },
-            {text: 'C. Number', correct: false },
-            {text: 'D. true / false', correct: true },
+        answers: [
+            { text: 'A. Undefined', correct: false },
+            { text: 'B. NaN', correct: false },
+            { text: 'C. Number', correct: false },
+            { text: 'D. true / false', correct: true },
         ]
     },
     {
-        question: "How do you select all tag elements using Js. ?",
-        answer:[
-            {text:'A. getElementById()', correct: false },
-            {text:'B. querySelectorAll()', correct: true },
-            {text:'C. querySelector()', correct: false },
-            {text:'D. None of the above', correct: false },
+        question: "How do you select ALL tag elements using Js. ?",
+        answers: [
+            { text: 'A. getElementById()', correct: false },
+            { text: 'B. querySelectorAll()', correct: true },
+            { text: 'C. querySelector()', correct: false },
+            { text: 'D. None of the above', correct: false },
         ]
     },
     {
         question: "What elements MUST you have in HTML ?",
-        answer:[
-            {text:"A. body, html, title, head, DOCTYPE, header", correct: false },
-            {text:"B. body, main, title, head, DOCTYPE, header", correct: false },
-            {text:"C. head, html, DOCTYPE, body, title, meta", correct: true },
-            {text:"D. title, html, body, head, link, footer", correct: false },
+        answers: [
+            { text: "A. body, html, title, head, DOCTYPE, header", correct: false },
+            { text: "B. body, main, title, head, DOCTYPE, header", correct: false },
+            { text: "C. head, html, DOCTYPE, body, title, meta", correct: true },
+            { text: "D. title, html, body, head, link, footer", correct: false },
+        ]
+    },
+    {
+        question: "What elements MUST you have in HTML ?",
+        answers: [
+            { text: "A. body, html, title, head, DOCTYPE, header", correct: false },
+            { text: "B. body, main, title, head, DOCTYPE, header", correct: false },
+            { text: "C. head, html, DOCTYPE, body, title, meta", correct: true },
+            { text: "D. title, html, body, head, link, footer", correct: false },
         ]
     },
 ];
+
+function thatsIt() {
+    countDown.classList.add('hide')
+    timerText.classList.add('hide')
+    questionEl.classList.add('hide')
+    answerBtn.classList.add('hide')
+    var startTimeTwo = setInterval(function () {
+        timeTwo--;
+        countDown.innerHTML = timeTwo
+        if (time === 0) {
+            clearInterval(startTimeTwo);
+        }
+        quizScor()
+    }, 1000);
+    
+}
+
+function quizScor(){
+    setScore.classList.remove('hide')
+    span.innerText = quizeScore;
+}
